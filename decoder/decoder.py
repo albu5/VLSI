75aaffc4399332f718dea5341926fd417dae2149
John Doe <shashankgangrade@gmail.com>
2015-10-04 15:28:37 +0530

    version 2

--- decoder/decoder.py
+++ decoder/decoder.py
@@ -1,3 +1,34 @@
 def function = decoder(kwargs):
 	return graph
 
+# EE677 VLSI Project 
+# Graph for implementation of generic Decoder
+# Written by Shashank Gangrade 
+# The logic cicuit of Encoder is one layer of NOT Gates for each inputs, followed by a layer of 
+
+#!/usr/bin/python
+import math
+
+n = 4;
+# Specify the size of decoder
+# n bits input 2^n bits output
+
+print (n)
+
+nMatrix = n+n+(1<<n);
+
+matrix  = [[-1]*nMatrix]*nMatrix
+
+c = '{0:04b}'.format(12);
+#Use this to genrate strings
+
+c = map(int, c) ;
+# Convert string to list
+
+
+
+
+
+
+
+
