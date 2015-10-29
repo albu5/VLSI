428008a97460b8b88b5cd70035c5394ce28bf4a0
Sagun Pai <sagung.pai@gmail.com>
2015-10-29 16:05:47 +0530

    dag added

--- encoder/encoder_circuit.py
+++ encoder/encoder_circuit.py
@@ -6,7 +6,7 @@
 import math
 
 # n-bit encoder assumed, logn outputs
-n = 16
+n = 8
 k = int(math.log(n,2))
 output = [[0]*k]*n
 encoder_circuit = [[] for x in range(k)]
@@ -25,11 +25,25 @@ for entry_index, entry in enumerate(output):
 			if entry_index not in encoder_circuit[i]:
 				encoder_circuit[i].append(entry_index)
 
-print encoder_circuit
+#print encoder_circuit
 
 # encoder_circuit stores various outputs in terms of input signals that need to be fed into an or gate.
 # k outputs, each with n/2 inputs connected to an OR gate
 
+from pprint import pprint
 
+nodes = n+k
+dag_graph = [[-1 for s in range(nodes)] for s in range(nodes)]
+# 0 to n-1 we have inputs
+# n to n+k-1 we have outputs (or gates)
+
+
+for i,e in enumerate(reversed(encoder_circuit)):
+	for j in e:
+		dag_graph[i+n][j] = 4
+		dag_graph[j][i+n] = 4
+
+
+pprint (dag_graph)
 
 
